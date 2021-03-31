e75ae6e23f39a1b62b973e8fa6c4146afa2bc629
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-03-31 12:57:23 +0530

    minor fix

--- Preprocessing/preprocess.py
+++ Preprocessing/preprocess.py
@@ -2,5 +2,5 @@ from resemblyzer import preprocess_wav
 
 def preprocess_audio(wav_data, source_sr):
     wav_data = wav_data.astype('float64')
-    wav = preprocess_wav(wav_data, source_sr = samplerate)
+    wav = preprocess_wav(wav_data, source_sr = source_sr)
     return wav
