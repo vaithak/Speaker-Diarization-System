38632db55345e6a072dd683501576781ff8121ae
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-04-10 20:51:18 +0530

    add model

--- Embedding/__init__.py
+++ Embedding/__init__.py
@@ -1,7 +1,9 @@
 from .Embedding import concat_segs, get_STFTs, align_embeddings
+from .speech_embedder_net import SpeechEmbedder
 
 __all__ = [
     'concat_segs',
     'get_STFTs',
-    'align_embeddings'
+    'align_embeddings',
+    'SpeechEmbedder'
 ]
